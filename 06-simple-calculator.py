8751ef3a8ac2523efc92ff3e38974e5e60a19565
wangui-kayla <wangui.kayla@gmail.com>
2026-08-14 23:33:51 +0300

    Now I've done the bug fixes. Errors are accepted and handled much better WITHOUT THE PROGRAM CRASHING. And the calculator shows my program design thinking

--- 06-simple-calculator.py
+++ 06-simple-calculator.py
@@ -2,55 +2,62 @@ import time
 
 def get_details():
     list_operators = ("+", "-", "*", "/")
-    try:
-        first_number = round(float(input("Enter the First Number: ")), 2)
-        operator = input("Enter the Operator between (+  -  *  /): ")
-        if operator not in list_operators:
-            raise ValueError("Error! Choose between the listed operators:\n+ ~ Addition\n- ~ Subtraction\n* ~ Multiplication\n/ ~ Division")
-        second_number = round(float(input("Enter the Second Number: ")), 2)
-
-    except ValueError as err:
-        print(f"Error! {err}")
-
-    return first_number, operator, second_number
-
-def calculations(first_number, operator, second_number):
     while True:
         try:
-            if operator == "+":
-                print("Calculating...")
-                time.sleep(1)
-                result = first_number + second_number
-            elif operator == "-":
-                print("Calculating...")
-                time.sleep(1)
-                result = first_number - second_number
-            elif operator == "*":
-                print("Calculating...")
-                time.sleep(1)
-                result = first_number * second_number
-            elif operator == "/":
-                print("Calculating...")
-                time.sleep(1)
-                result = first_number / second_number
+            first_number = float(input("Enter the First Number: "))
+            operator = input("Enter the Operator between (+  -  *  /): ")
+            if operator not in list_operators:
+                raise ValueError("Error! Choose between the listed operators:\n+ ~ Addition\n- ~ Subtraction\n* ~ Multiplication\n/ ~ Division")
+            second_number = float(input("Enter the Second Number: "))
 
-        except ZeroDivisionError as err:
-            print(f"Error! {err}")
+            return first_number, operator, second_number
+
+        except ValueError as err:
+            print(f"{err}")
+
+def calculations(first_number, operator, second_number):
+    try:
+        if operator == "+":
+            print("Calculating...")
+            time.sleep(1)
+            result = first_number + second_number
+        elif operator == "-":
+            print("Calculating...")
+            time.sleep(1)
+            result = first_number - second_number
+        elif operator == "*":
+            print("Calculating...")
+            time.sleep(1)
+            result = first_number * second_number
+        elif operator == "/":
+            print("Calculating...")
+            time.sleep(1)
+            result = first_number / second_number
 
         return result
+
+    except ZeroDivisionError as err:
+        print(f"{err}")
+        return None
 
 calculating = True
 while calculating:
     print("Welcome to the Calculator!")
     time.sleep(1)
 
-    first_num, op, second_num = get_details()
+    while True:
+        first_num, op, second_num = get_details()
 
-    answer = calculations(first_num, op, second_num)
+        answer = calculations(first_num, op, second_num)
+
+        if answer is None:
+            continue
+        else:
+            break
 
     print("Result...")
     time.sleep(1)
-    print(f"{first_num} {op} {second_num} = {answer}")
+    print(f"{first_num:.2f} {op} {second_num:.2f} = {answer:.2f}")
 
     while True:
         retry = input("Would you like to calculate again? (Y/N): ").upper()
